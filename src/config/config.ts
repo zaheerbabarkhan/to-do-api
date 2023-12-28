661c20c14f2a3fbcef2fd22e0a773a603353a8db
zaheerbabarkhan <zaheerbaberkhan@gmail.com>
2023-12-28 16:25:17 +0500

    Initial config added i.e typescript config, nodemon, env vars, status-check route

---
+++ src/config/config.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv"
+
+
+dotenv.config();
+
+
+export default {
+    PORT: process.env.PORT,
+    HOST: process.env.HOST
+}
---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express"
+import config from "./config/config";
+
+const app = express();
+
+app.get("/status-check", (req, res) => {
+    res.send("Health check successfull")
+})
+
+
+app.listen(config.PORT, () => {
+    console.log(`API server is listening on the http://${config.HOST}:${config.PORT}`)
+})
