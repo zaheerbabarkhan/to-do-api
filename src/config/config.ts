d0d2bea91e49c0c9a800b9b6657c094041349105
zaheerbabarkhan <zaheerbaberkhan@gmail.com>
2023-12-28 18:16:34 +0500

    added linting to the project.

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 
 dotenv.config();
@@ -7,4 +7,4 @@ dotenv.config();
 export default {
     PORT: process.env.PORT,
     HOST: process.env.HOST
-}
+};
