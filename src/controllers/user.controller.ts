00f5a34c1a597df62893ab8d8c250aecc2fd1c06
zaheerbabarkhan <zaheerbaberkhan@gmail.com>
2024-01-05 16:36:29 +0500

    http error class added and the user email existing check added

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,24 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { User } from "../database/models";
 import bcrypt from "bcrypt";
 import config from "../config/config";
+import { HttpError } from "../errors/http.error";
+import httpStatus from "http-status";
 
-export const createUser = async (req: Request, res: Response) => {
+export const createUser = async (req: Request, res: Response, next: NextFunction) => {
     
     const {firstName, lastName, email, password} = req.body;
+
+    const userExist = await User.count({
+        where: {
+            email,
+        }
+    });
+
+    if (userExist) {
+        next(new HttpError(httpStatus.BAD_REQUEST, "Email already registered"));
+        return;
+    }
 
     const passwordHash = await bcrypt.hash(password, config.BCRYPT.SALT_ROUNDS);
 
