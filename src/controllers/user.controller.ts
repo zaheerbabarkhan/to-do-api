90f415b2c174bba6bd469d45e6f2a81652aeb53d
zaheerbabarkhan <zaheerbaberkhan@gmail.com>
2024-01-19 16:52:34 +0500

    redis connection added

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import UserService from "../services/user.services";
+import redis from "../database/redis";
+import httpStatus from "http-status";
 
 export const createUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const newUser = await UserService.createUser(req.body);
-        res.status(201).send(newUser);
+        res.status(httpStatus.CREATED).send(newUser);
 
     } catch (error) {
         next(error);
@@ -18,7 +20,7 @@ export const confirmUserEmail = async (req: Request, res: Response, next: NextFu
     const token = req.query.token;
 
     if (!token) {
-        res.status(401).json({
+        res.status(httpStatus.UNAUTHORIZED).json({
             message: "Unauthorized"
         });
         return;
@@ -26,7 +28,7 @@ export const confirmUserEmail = async (req: Request, res: Response, next: NextFu
     
     try {
         await UserService.confirmUserEmail(String(token));
-        res.status(200).json({
+        res.status(httpStatus.OK).json({
             message: "Email confirmed successfully."
         });
     } catch (error) {
@@ -44,9 +46,21 @@ export const userLogin = async (req: Request, res: Response, next: NextFunction)
             email,
             password,
         });
-        res.status(200).json(responseBody);
+        res.status(httpStatus.OK).json(responseBody);
     } catch (error) {
         next(error);
     }
     
 };
+
+
+export const  userLogout = async (req: Request, res: Response, next: NextFunction) => { 
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+        res.status(httpStatus.UNAUTHORIZED).json({
+            message: "Unauthorized"
+        });
+        return;
+    }
+    redis.sAdd()
+}
---
+++ src/database/redis.ts
@@ -0,0 +1,22 @@
+// redisClient.ts
+import * as redis from "redis";
+
+let client: redis.RedisClientType;
+
+function createClient(): redis.RedisClientType {
+    if (!client) {
+        client = redis.createClient();
+
+        client.on("connect", () => {
+            console.log("Connected to Redis");      
+        });
+
+        client.on("error", (err: unknown) => {
+            console.error(`Error connecting to Redis: ${err}`);
+        });
+    }
+
+    return client;
+}
+
+export default createClient();
