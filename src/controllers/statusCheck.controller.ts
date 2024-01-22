b4cc21c51a3c01ee61c34fc4935030f8560872af
zaheerbabarkhan <zaheerbaberkhan@gmail.com>
2024-01-22 12:43:24 +0500

    status code changed to code readability

--- src/controllers/statusCheck.controller.ts
+++ src/controllers/statusCheck.controller.ts
@@ -1,15 +1,17 @@
 import { Response, Request } from "express";
 import sequelize from "../database/init";
+import httpStatus from "http-status";
+
 export const healthCheck = async (_req: Request, res: Response) => {
     const db = sequelize();
     const [result] = await db.query("SELECT 1+1 as result");
     if(!result) {
-        res.status(500).send("Internal Server Error");
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send("Internal Server Error");
     }
     const data = {
         uptimeInSeconds: Math.floor(process.uptime()),
         message: "Ok",
         date: new Date()
     };
-    res.status(200).send(data);
+    res.status(httpStatus.OK).send(data);
 };
